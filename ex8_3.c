8624f1767775510ff246011392324513a47b0ed6
tiagooayala <tiagooayala@gmail.com>
2026-05-28 16:54:20 -0300

    ex 3 B
    resolucao do ex C, lista 8, parte 3

--- ex8_3.c
+++ ex8_3.c
@@ -1,1 +1,17 @@
-dsadasmdasdoasokdsakodsaok
+#include<stdio.h>
+#include<string.h>
+int somatoria(int n, int *soma){
+    *soma = 0;
+    for(int i=0;i<=n;i++){
+        *soma = *soma + i;
+    }
+}
+
+int main(){
+    int num, total;
+    printf("Digite um numero para somatoria: ");
+    scanf("%i", &num);
+    somatoria(num, &total);
+    printf("O resultado e: %i", total);
+    return 0;
+}
